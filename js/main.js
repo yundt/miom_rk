7b99d1c6bf0c06fb90bc8b5c007589a6dea0cf4e
Marek Nowak <mareknowakk@gmail.com>
2014-11-16 00:19:22 +0100

    Tracks store links.

--- js/main.js
+++ js/main.js
@@ -1,5 +1,16 @@
 $dom.onready( function() {
   new Parallax(document.getElementById('scene'));
 
-  mixpanel.track("Page view");
+  if(document.URL.match(/miomrk\.com/)) {
+    mixpanel.track("Page view");
+
+    var storeIds = [
+      'spotify', 'itunes', 'amazon','japanese_amazon',
+      'google_play', 'deezer', 'rdio'
+    ];
+
+    for(var i = 0; i < storeIds.length; i++) {
+      mixpanel.track_links("#" + storeIds[i], "Clicked " + storeIds[i]);
+    }
+  }
 });
